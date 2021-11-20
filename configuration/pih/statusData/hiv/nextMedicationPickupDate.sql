57e55a91e5b7a94a4b1171ce1c8a4b3ea0023c57
Michael Seaton <mseaton@pih.org>
2021-11-20 14:12:24 -0500

    UHM-5950:  Bug fix to med pickup query, simplifying data point access, move comments per code review.

--- configuration/pih/statusData/hiv/nextMedicationPickupDate.sql
+++ configuration/pih/statusData/hiv/nextMedicationPickupDate.sql
@@ -1,27 +1,28 @@
-# Retrieve the  Next Medication Pickup Date (PIH:5096) from the last HIV drug dispensing encounter.
-#   If this date is more than 28 days ago, highlight it in red.
-#   If there have been no HIV Drug Dispensing encounters for this patient, display “No medication pickups” in red.
+# This query retrieves the total number of HIV Dispensing Encounters and the Most Recent Scheduled Dispensing Date for a patient
 
-set @hivDispensingEncounterType = encounter_type('${encounterType.HIV_DISPENSING.uuid}');
-select q1.num_pickups as numPickups, q2.value_datetime as nextPickupDate
-from
-    (
-        select count(e.encounter_id) as num_pickups
-        from encounter e
-        where e.patient_id = @patientId
-          and e.encounter_type = @hivDispensingEncounterType
-          and e.voided = 0
-    ) q1,
-    (
-        select o.value_datetime
-        from obs o
-        inner join encounter e on o.encounter_id = e.encounter_id
-        where o.concept_id = concept_from_mapping('PIH','5096')
-          and e.encounter_type = @hivDispensingEncounterType
-          and o.voided = 0
-          and e.voided = 0
-          and o.person_id = @patientId
-        order by e.encounter_datetime desc
-        limit 1
-    ) q2
+set @hivDispensingEncounterType = encounter_type('cc1720c9-3e4c-4fa8-a7ec-40eeaad1958c');
+select ifnull(q1.num_pickups, 0) as numPickups, q2.value_datetime as nextPickupDate
+from patient p
+left join
+     (
+         select e.patient_id, count(e.encounter_id) as num_pickups
+         from encounter e
+         where e.encounter_type = @hivDispensingEncounterType
+           and e.voided = 0
+           and e.patient_id = @patientId
+     ) q1 on p.patient_id = q1.patient_id
+left join
+     (
+         select o.person_id, o.value_datetime
+         from obs o
+                  inner join encounter e on o.encounter_id = e.encounter_id
+         where o.concept_id = concept_from_mapping('PIH', '5096')
+           and e.encounter_type = @hivDispensingEncounterType
+           and o.voided = 0
+           and e.voided = 0
+           and o.person_id = @patientId
+         order by e.encounter_datetime desc
+         limit 1
+     ) q2 on p.patient_id = q2.person_id
+where p.patient_id = @patientId
 ;
