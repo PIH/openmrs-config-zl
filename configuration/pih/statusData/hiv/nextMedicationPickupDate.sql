fc60e1604a6902cb8eeec38e744ca07491d9264b
Michael Seaton <mseaton@pih.org>
2021-11-22 15:17:19 -0500

    UHM-5950: Attempt to simplify some of the queries

--- configuration/pih/statusData/hiv/nextMedicationPickupDate.sql
+++ configuration/pih/statusData/hiv/nextMedicationPickupDate.sql
@@ -1,28 +1,29 @@
 # This query retrieves the total number of HIV Dispensing Encounters and the Most Recent Scheduled Dispensing Date for a patient
 
-set @hivDispensingEncounterType = encounter_type('${encounterType.HIV_DISPENSING.uuid}');
-select ifnull(q1.num_pickups, 0) as numPickups, q2.value_datetime as nextPickupDate
-from patient p
-left join
-     (
-         select e.patient_id, count(e.encounter_id) as num_pickups
-         from encounter e
-         where e.encounter_type = @hivDispensingEncounterType
-           and e.voided = 0
-           and e.patient_id = @patientId
-     ) q1 on p.patient_id = q1.patient_id
-left join
-     (
-         select o.person_id, o.value_datetime
-         from obs o
-                  inner join encounter e on o.encounter_id = e.encounter_id
-         where o.concept_id = concept_from_mapping('PIH', '5096')
-           and e.encounter_type = @hivDispensingEncounterType
-           and o.voided = 0
-           and e.voided = 0
-           and o.person_id = @patientId
-         order by e.encounter_datetime desc
-         limit 1
-     ) q2 on p.patient_id = q2.person_id
-where p.patient_id = @patientId
+set @hivDispensingEncounterType = encounter_type('cc1720c9-3e4c-4fa8-a7ec-40eeaad1958c');
+
+# Get total number of dispensing encounters for the patient
+
+select ifnull(count(e.encounter_id), 0) into @numPickups
+from encounter e
+where e.encounter_type = @hivDispensingEncounterType
+  and e.voided = 0
+  and e.patient_id = @patientId;
+
+# Get the next scheduled appointment date from the last dispensing encounter
+
+select o.value_datetime into @nextPickupDate
+from obs o
+inner join encounter e on o.encounter_id = e.encounter_id
+where o.concept_id = concept_from_mapping('PIH', '5096')
+  and e.encounter_type = @hivDispensingEncounterType
+  and o.voided = 0
+  and e.voided = 0
+  and o.person_id = @patientId
+order by e.encounter_datetime desc
+limit 1;
+
+select
+       ifnull(@numPickups, 0) as numPickups,
+       date(@nextPickupDate) as nextPickupDate
 ;
