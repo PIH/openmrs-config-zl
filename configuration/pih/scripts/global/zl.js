8cabdd11c0c0ddb6d89d5bab0049c123c0d31fa9
louidorjp <jpaullouidor@gmail.com>
2022-07-12 15:01:21 -0400

    HAI-697: Encounter date instead of current used

--- configuration/pih/scripts/global/zl.js
+++ configuration/pih/scripts/global/zl.js
@@ -184,26 +184,32 @@ function setUpObsWithObsDateTime(widgetId){
     }
 }
 
-function returnVisitDateValidation(badReturnVisitDateMsg){
+function returnVisitDateValidation(encounterDate,returnVisitDateLessThanEncounterDateMsg,badReturnVisitDateMsg){
 
-   let domEl= jq("#apptDate").find("input[type=text]");
-   domEl.datepicker('option','yearRange', '+0:+1'); 
+   const domEl= jq("#apptDate").find("input[type=text]");
+   const yrRange = encounterDate.getFullYear() + ":" + (new Date().getFullYear() + 1);
+   domEl.datepicker('option','yearRange', yrRange); 
    domEl.prop("readonly", "readonly"); 
-  
-  jq(domEl).change(function(){
 
+  jq(domEl).change(function(){
     const nextVisitDate = domEl.datepicker('getDate');
-    const currentDate =new Date();
-    const months = (nextVisitDate.getFullYear() - currentDate.getFullYear()) * 12;
-    const nextMonth= nextVisitDate.getMonth();
-    const currentMonth = currentDate.getMonth();
-    
-    if(months <=12 && currentMonth >= nextMonth && nextVisitDate > currentDate){
+    const months = (nextVisitDate.getFullYear() - encounterDate.getFullYear()) * 12;
+
+    if(months < 12  && nextVisitDate > encounterDate){
       jq("#return-visit-date-error-message").text('')
       jq('.submit').prop("disabled", false);
-    }else{
+    }
+    else if(months == 12 && nextVisitDate.getMonth() <= encounterDate.getMonth()){
+      jq("#return-visit-date-error-message").text('')
+      jq('.submit').prop("disabled", false);
+    }else if(months <= 0 && encounterDate > nextVisitDate){
+        jq("#return-visit-date-error-message").text(returnVisitDateLessThanEncounterDateMsg)
+        jq('.submit').prop("disabled", true);
+    }
+    else{
       jq("#return-visit-date-error-message").text(badReturnVisitDateMsg)
       jq('.submit').prop("disabled", true);
     }
+
   })        
 }
