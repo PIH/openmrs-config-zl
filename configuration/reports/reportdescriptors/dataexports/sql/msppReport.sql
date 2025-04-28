80cb8e4ae5cf53e1e780e6badb412358363f63e3
Dutervil <dwadson3@gmail.com>
2025-04-28 11:42:05 -0400

    section 2) EXAMENS DE LABORATOIRE msppReport

--- configuration/reports/reportdescriptors/dataexports/sql/msppReport.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/msppReport.sql
@@ -253,6 +253,318 @@ AND o.voided =0
 AND date(e.encounter_datetime) >= @startDate
 AND date(e.encounter_datetime) < @endDate;
 
+-- EXAMEN LABORATOIRE
+
+ DROP TABLE IF EXISTS  pregnancy_patient_temp;
+CREATE TEMPORARY TABLE pregnancy_patient_temp(person_id int,pregnant int);
+ INSERT INTO pregnancy_patient_temp(person_id,pregnant)
+   SELECT
+    o.person_id, IF(DATE(o.value_datetime) > CURDATE(), 1, 0)
+    FROM obs o
+    WHERE o.concept_id = concept_from_mapping('PIH', '5596')
+    AND o.voided = 0
+    AND DATE(o.value_datetime) > CURDATE()
+    GROUP BY o.person_id;
+
+
+-- TOTAL TEST D'URINE
+SELECT COUNT(*) INTO @total_urine_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '12375')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+
+
+-- TOTAL TEST D'HEMOGRAME
+SELECT COUNT(*) INTO @total_hemogram_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '11711')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+
+ -- TOTAL TEST MALARIA TEST RAPID
+SELECT COUNT(*) INTO @totat_malaria_rapd_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '11464')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+  -- TOTAL TEST RPR
+SELECT COUNT(*) INTO @total_rpr_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', 'RPR')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+ 
+  -- TOTAL TEST  HIV
+SELECT COUNT(*) INTO @total_hiv_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '1040')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+  -- TOTAL SICKLING TEST
+SELECT COUNT(*) INTO @total_sickling_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '12716')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+ 
+  -- TOTAL TEST GROUPE SANGUIN
+SELECT COUNT(*) INTO @total_blood_type_test_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', 'BLOOD TYPING')
+  AND pr.pregnant=1
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+   
+ 
+
+-- Data for not Pregnant Women
+ 
+  -- TOTAL TEST GROUPE SANGUIN FOR NOT PREGNANT
+SELECT COUNT(*) INTO @total_blood_type_test
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', 'BLOOD TYPING')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+  -- TOTAL SICKLING TEST NOT PREGNANT
+SELECT COUNT(*) 
+INTO @total_sickling_test
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '12716')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate;
+
+  
+  
+ -- TOTAL TEST MALARIA TEST RAPID NOT PREGNANT
+
+ SELECT COUNT(*) 
+INTO @total_malaria_rapd_test
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '11464')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+
+
+
+  
+ -- TOTAL TEST MALARIA TEST RAPID POSITIVE NOT PREGNANT
+SELECT COUNT(*) INTO @totat_malaria_rapd_test_positif
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '11464')
+  AND concept_name(o.value_coded, 'fr') = 'Positif'
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+ 
+ -- TOTAL TEST MALARIA TEST RAPID POSITIVE
+SELECT COUNT(*) INTO @totat_malaria_rapd_test_positif_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '11464')
+  AND pr.pregnant=1
+  AND concept_name(o.value_coded, 'fr') = 'Positif'
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+  -- TOTAL TEST RPR POSITIVE
+ SELECT COUNT(*) INTO @total_rpr_test_positif_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', 'RPR')
+  AND pr.pregnant=1
+  AND concept_name(o.value_coded, 'fr') = 'Positif'
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+ 
+ 
+  -- TOTAL TEST  HIV POSITIVE
+SELECT COUNT(*) INTO @total_hiv_test_positif_women
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+INNER JOIN pregnancy_patient_temp pr
+    ON p.person_id = pr.person_id
+WHERE o.concept_id = concept_from_mapping('PIH', '1040')
+  AND pr.pregnant=1
+  AND concept_name(o.value_coded, 'fr') = 'Positif'
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+
+--  FOR Malaria Test microscopique
+ 
+ SELECT COUNT(1) INTO @malaria_test_microscopique_vivax  
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE concept_name(o.value_coded, 'fr') = 'Positive for Plasmodium vivax'
+  AND o.concept_id = concept_from_mapping('PIH', '20079')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+ 
+ 
+SELECT COUNT(1) INTO @malaria_test_microscopique_oval  
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE concept_name(o.value_coded, 'fr') = 'Positive for Plasmodium ovale'
+  AND o.concept_id = concept_from_mapping('PIH', '20079')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+
+SELECT COUNT(1) INTO @malaria_test_microscopique_malariae 
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+WHERE concept_name(o.value_coded, 'fr') = 'Positive for Plasmodium malariae'
+ AND o.concept_id = concept_from_mapping('PIH', '20079')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+SELECT COUNT(1) INTO @malaria_test_microscopique_falciparum 
+FROM obs o 
+INNER JOIN encounter e 
+    ON o.encounter_id = e.encounter_id
+INNER JOIN person p
+    ON p.person_id = o.person_id
+ WHERE concept_name(o.value_coded, 'fr') = 'Positive for Plasmodium falciparum'
+  AND o.concept_id = concept_from_mapping('PIH', '20079')
+  AND o.voided = 0 
+  AND e.voided = 0
+  AND date(e.encounter_datetime) >= @startDate
+  AND date(e.encounter_datetime) < @endDate;
+ 
+SELECT  (@malaria_test_microscopique_vivax +
+         @malaria_test_microscopique_oval +
+         @malaria_test_microscopique_malariae +
+         @malaria_test_microscopique_falciparum
+         ) INTO @malaria_test_microscopique_total ;
+         
+
 
 
 SELECT SUM(child_under_1_n) "CHILD_UNDER_1_N",SUM(child_under_1_s) "CHILD_UNDER_1_S",
@@ -267,5 +579,35 @@ SELECT SUM(child_under_1_n) "CHILD_UNDER_1_N",SUM(child_under_1_s) "CHILD_UNDER_
             @totalMentalDisorder 'PEOPLE_MENTAL_DISODER',
             0 'CLIENT_S_BU_DENTAIRE_N', 0 'CLIENT_S_BU_DENTAIRE_S',
             0 'PEOPLE_REDUCED_MOB_MOTOR_N',0 'PEOPLE_REDUCED_MOB_MOTOR_S',
-            0 'PEOPLE_REDUCED_MOB_SENSORY_N',0 'PEOPLE_REDUCED_MOB_SENSORY_S'
+            0 'PEOPLE_REDUCED_MOB_SENSORY_N',0 'PEOPLE_REDUCED_MOB_SENSORY_S',
+             @total_urine_test_women 'TOTAL_URINE_TEST_WOMEN',
+            @total_hemogram_test_women 'TOTAL_HEMOGRAM_TEST_WOMEN',
+            @totat_malaria_rapd_test_women 'TOTAL_MALARIA_RAPD_TEST_WOMEN',
+            @totat_malaria_rapd_test_positif_women 'TOTAL_MALARIA_RAPD_TEST_POSITIF_WOMEN',
+          
+            @total_rpr_test_women 'TOTAL_RPR_TEST_WOMEN',
+            @total_rpr_test_positif_women 'TOTAL_RPR_TEST_POSITIF_WOMEN',
+            
+            @total_hiv_test_women 'TOTAL_HIV_TEST_WOMEN',
+            @total_hiv_test_positif_women 'TOTAL_HIV_TEST_POSITIF_WOMEN',
+            
+            @total_sickling_test_women 'TOTAL_SICKLING_TEST_WOMEN',
+            @total_sickling_test 'TOTAL_SICKLING_TEST',
+              
+            @total_blood_type_test_women 'TOTAL_BLOOD_TYPE_TEST_WOMEN',
+            @total_blood_type_test 'TOTAL_BLOOD_TYPE_TEST',
+            
+            @total_blood_type_test 'TOTAL_BLOOD_TYPE_TEST',
+            @total_sickling_test 'TOTAL_SICKLING_TEST',
+            @total_malaria_rapd_test 'TOTAL_MALARIA_RAPD_TEST',
+            @totat_malaria_rapd_test_positif 'TOTAL_MALARIA_RAPD_TEST_POSITIF',
+            
+            @total_malaria_rapd_test 'TOTAL_MALARIA',
+            @totat_malaria_rapd_test_positif 'TOTAL_MALARIA_POSITIF',
+           
+            @malaria_test_microscopique_vivax 'VIVAX',
+            @malaria_test_microscopique_oval'OVAL',
+            @malaria_test_microscopique_malariae 'MALARIAE',
+            @malaria_test_microscopique_falciparum 'FALCIPARUM',
+            @malaria_test_microscopique_total 'MALARIAN_TEST_MICRO'
        FROM visits_distribution_temp;
