a50ae19ca36e734f79196eb0c3a0ed8c767e0fa9
Dutervil <dwadson3@gmail.com>
2025-09-19 13:03:02 -0400

    uptimizing the query

--- configuration/pih/statusData/mch/prenatalVisit.sql
+++ configuration/pih/statusData/mch/prenatalVisit.sql
@@ -7,7 +7,7 @@ FROM obs o
 INNER JOIN encounter e ON o.encounter_id = e.encounter_id
 INNER JOIN person p ON p.person_id = o.person_id
 LEFT JOIN (
-   SELECT  value_coded,person_id,encounter_datetime from obs as o2
+   SELECT  value_coded,person_id,encounter_datetime,e2.encounter_id from obs as o2
    JOIN encounter e2 
    ON e2.encounter_id=o2.encounter_id
    WHERE value_coded =concept_from_mapping('PIH', '6259')
