cb248e93ed3a49a48022c99aea1391f909d4b911
Dutervil <dwadson3@gmail.com>
2025-10-06 11:21:01 -0400

    Optimized query by removing subquery and improving performance using date range and index.

--- configuration/pih/statusData/mch/deliveryDate.sql
+++ configuration/pih/statusData/mch/deliveryDate.sql
@@ -1,12 +1,9 @@
-SELECT date(o.value_datetime) as estimatedDateOfConfinement
-FROM obs o 
-WHERE o.obs_id = (
-    SELECT o2.obs_id 
-    FROM obs o2 
-    WHERE o2.person_id = @patientId
-      AND o2.concept_id = concept_from_mapping('PIH','ESTIMATED DATE OF CONFINEMENT')
-      AND o2.voided = 0
-      AND DATEDIFF(NOW(), o2.obs_datetime) <= 270
-    ORDER BY o2.obs_datetime DESC 
-    LIMIT 1
-);
+
+SELECT DATE(o.value_datetime) AS estimatedDateOfConfinement
+FROM obs o
+WHERE o.person_id = @patientId
+  AND o.concept_id = concept_from_mapping('PIH', 'ESTIMATED DATE OF CONFINEMENT')
+  AND o.voided = 0
+  AND o.obs_datetime >= DATE_SUB(NOW(), INTERVAL 270 DAY)
+ORDER BY o.obs_datetime DESC
+LIMIT 1;
