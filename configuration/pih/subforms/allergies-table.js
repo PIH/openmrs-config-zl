26ef0bbaf554582b1648a772df7eb071296417c2
Michael Seaton <mseaton@pih.org>
2026-04-01 10:52:42 -0400

    UHM-9193 - Add patient allergies the HIV dispensing form

---
+++ configuration/pih/subforms/allergies-table.js
@@ -0,0 +1,36 @@
+/**
+ * This subform is entirely javascript-based on works via convention over configuration
+ * It looks for elements with specific classes in the form, and will populate them with the patient's allergy information
+ */
+jq(document).ready(function() {
+    const allergiesTable = jq(".allergies-table-subform .allergies-table");
+    const allergiesTableBody = allergiesTable.find("tbody");
+    if (allergiesTableBody.length > 0) {
+        allergiesTableBody.empty();
+        const patientUuid = '<lookup expression="patient.uuid"/>';
+        const rep = "custom:(display,reactions:(reaction:(display),reactionNonCoded),severity:(display),comment,auditInfo:(dateChanged,dateCreated))";
+        jq.get(openmrsContextPath + "/ws/rest/v1/patient/" + patientUuid + "/allergy?v=" + rep, function(data, textStatus, jqXHR) {
+            const status = jqXHR.status;
+            const allergies = data?.results ?? [];
+            console.log(allergies);
+            if (allergies.length === 0) {
+                const message = status === 204 ? '<uimessage code="allergyui.unknown"/>' : '<uimessage code="allergyui.noKnownAllergies"/>';
+                console.log(message);
+                const noAllergiesRow = jq("<tr/>");
+                noAllergiesRow.append(jq("<td/>").attr("colspan", "5").html(message));
+                allergiesTableBody.append(noAllergiesRow);
+            }
+            else {
+                allergies.forEach(a => {
+                    const row = jq("<tr/>");
+                    row.append(jq("<td/>").addClass("allergy-allergen-column").html(a.display));
+                    row.append(jq("<td/>").addClass("allergy-reaction-column").html((a.reactions ?? []).map(r => { return r.reaction?.display ?? r.reactionNonCoded ?? "" }).join(", ")));
+                    row.append(jq("<td/>").addClass("allergy-severity-column").html(a.severity?.display ?? ""));
+                    row.append(jq("<td/>").addClass("allergy-comment--column").html(a.comment ?? ""));
+                    row.append(jq("<td/>").addClass("allergy-last-updated-column").html(moment(a.auditInfo.dateChanged ?? a.auditInfo.dateCreated).format("DD MMM YYYY")));
+                    allergiesTableBody.append(row);
+                });
+            }
+        });
+    }
+});
