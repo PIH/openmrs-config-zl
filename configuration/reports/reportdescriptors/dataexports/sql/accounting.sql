4119161c50114537689bdfc1fa9bc28531f7980e
Mark Goodrich <mgoodrich@pih.org>
2026-07-21 14:48:44 -0400

    HAI-1205: Segregate Monitoring Reports by Location (#447)

--- configuration/reports/reportdescriptors/dataexports/sql/accounting.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/accounting.sql
@@ -4,48 +4,60 @@ SET @endDate = ADDDATE(@endDate, INTERVAL 1 DAY);
 
 select cn.name "Catégorie", CEIL(sum(o.value_numeric)) "Montant" from
 obs o
-INNER JOIN obs os on os.encounter_id = o.encounter_id and os.voided = 0 and os.concept_id = 
-    (select concept_id from report_mapping where source = 'PIH' and code = 'Type of HUM visit') 
-LEFT OUTER JOIN concept_name cn on cn.concept_id = os.value_coded and cn.locale = 'fr' and cn.locale_preferred = '1'  and cn.voided = 0    
+INNER JOIN obs os on os.encounter_id = o.encounter_id and os.voided = 0 and os.concept_id =
+    (select concept_id from report_mapping where source = 'PIH' and code = 'Type of HUM visit')
+LEFT OUTER JOIN concept_name cn on cn.concept_id = os.value_coded and cn.locale = 'fr' and cn.locale_preferred = '1'  and cn.voided = 0
+INNER JOIN encounter e ON o.encounter_id = e.encounter_id AND e.voided = 0
+INNER JOIN visit v ON e.visit_id = v.visit_id AND v.voided = 0
 where 1=1
 and o.voided = 0
-and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount') 
+and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount')
 AND date(o.obs_datetime) >=  @startDate
 AND date(o.obs_datetime) <  @endDate
+AND v.location_id = @location
 group by cn.name
 UNION ALL
-select ' ',' ' 
+select ' ',' '
 UNION ALL
   select 'Total pour cette période', CEIL(sum(o.value_numeric)) "Montant" from
 obs o
+INNER JOIN encounter e ON o.encounter_id = e.encounter_id AND e.voided = 0
+INNER JOIN visit v ON e.visit_id = v.visit_id AND v.voided = 0
 where 1=1
 and o.voided = 0
-and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount') 
+and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount')
 AND date(o.obs_datetime) >=  @startDate
 AND date(o.obs_datetime) < @endDate
+AND v.location_id = @location
 UNION ALL
-select ' ',' ' 
+select ' ',' '
 UNION ALL
 select 'Nombre de patients exonérés', CEIL(count(o.person_id)) from
 obs o
+INNER JOIN encounter e ON o.encounter_id = e.encounter_id AND e.voided = 0
+INNER JOIN visit v ON e.visit_id = v.visit_id AND v.voided = 0
 where 1=1
 and o.voided = 0
-and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount') 
+and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount')
 and o.value_numeric = 0
 AND date(o.obs_datetime) >=  @startDate
 AND date(o.obs_datetime) < @endDate
+AND v.location_id = @location
 UNION ALL
-select ' ',' ' 
+select ' ',' '
 UNION ALL
 select 'Nombre de patients ayant payés plus déune fois', CEIL(count(o.person_id)) from
 obs o
+INNER JOIN encounter e ON o.encounter_id = e.encounter_id AND e.voided = 0
+INNER JOIN visit v ON e.visit_id = v.visit_id AND v.voided = 0
 where 1=1
 and o.voided = 0
-and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount') 
+and o.concept_id = (select concept_id from report_mapping where source = 'PIH' and code = 'Payment amount')
 and o.value_numeric > 0
 AND date(o.obs_datetime) >=  @startDate
 AND date(o.obs_datetime) < @endDate
-and exists  
+AND v.location_id = @location
+and exists
   (select 1 from obs o2 
   where o2.obs_id <> o.obs_id
   and o2.concept_id = o.concept_id
